5abb58730d01f29dcb933f7f60b6c43a44b98c6d
VP <huuwulleop@hotmail.com>
2022-04-13 20:55:49 -0400

    Init form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useState } from 'react'
+
+// components
+import NoteForm from './components/NoteForm';
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <h1>Add a new note</h1>
+      <NoteForm />
+      
+      <h1>ToDo list</h1>
+    </div>
   )
 }
 
---
+++ src/components/NoteForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NoteForm = () => {
+    return (
+        <form>
+            <input placeholder="add a new note..." />
+            <button>Add</button>
+        </form>
+    )
+}
+
+export default NoteForm
