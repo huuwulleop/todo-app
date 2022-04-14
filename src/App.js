fd48612ef63907bebaa5bec7531771d6a1e834cd
VP <huuwulleop@hotmail.com>
2022-04-13 21:35:02 -0400

    Implement list of notes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 // components
 import NoteForm from './components/NoteForm';
+import Note from './components/Note';
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    { content: 'Do stuffs', id: 1 },
+    { content: 'Make things', id: 2 }
+  ])
   const [newNote, setNewNote] = useState('')
-  
+
   const addNote = (event) => {
     event.preventDefault()
     console.log('event is clicked');
@@ -16,13 +21,19 @@ const App = () => {
     console.log(event.target.value);
     setNewNote(event.target.value)
   }
-  
+
   return (
     <div>
       <h1>Add a new note</h1>
       <NoteForm addNote={addNote} newNote={newNote} changeNote={changeNote} />
-      
+
       <h1>ToDo list</h1>
+      <ul>
+        {notes.length > 0
+          ? notes.map(note => <li><Note content={note.content} /></li>)
+          : 'Everything is done! :D'
+        }
+      </ul>
     </div>
   )
 }
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Note = () => {
+const Note = ({ content }) => {
     return (
-        <div></div>
+        <div>{content}</div>
     )
 }
 
