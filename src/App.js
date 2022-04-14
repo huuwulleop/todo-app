a5f9d7d38b904e4dc6ba664dee61d8b3740a96ca
VP <huuwulleop@hotmail.com>
2022-04-13 21:07:11 -0400

    Handle input change

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import { useState } from 'react'
 import NoteForm from './components/NoteForm';
 
 const App = () => {
+  const [newNote, setNewNote] = useState('')
+  
   const addNote = (event) => {
     event.preventDefault()
     console.log('event is clicked');
+    setNewNote('')
+  }
+
+  const changeNote = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value)
   }
   
   return (
     <div>
       <h1>Add a new note</h1>
-      <NoteForm addNote={addNote} />
+      <NoteForm addNote={addNote} newNote={newNote} changeNote={changeNote} />
       
       <h1>ToDo list</h1>
     </div>
--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const NoteForm = ({ addNote }) => {
+const NoteForm = ({ addNote, newNote, changeNote }) => {
     return (
         <form onSubmit={addNote}>
-            <input placeholder="add a new note..." />
+            <input placeholder="add a new note..." value={newNote} onChange={changeNote} />
             <button type="submit">Add</button>
         </form>
     )
